7782b5205adaa1e87bc51615c0a56291b9145998
andrew.murphy <andrew.murphy@openjawtech.com>
2017-11-07 23:22:58 +0000

    test

--- java/src/TypeArray/OddsOrEvens.java
+++ java/src/TypeArray/OddsOrEvens.java
@@ -1,10 +1,13 @@
 package TypeArray;
 
 public class OddsOrEvens {
-
+//method to order an array by odd and even numbers
     public static void OddsOrEvens(int[] A){
         int nextEven = 0, nextOdd = A.length -1;
-        while(nextEven > nextOdd) {
+
+        System.out.println(nextOdd);
+        System.out.println(A[nextOdd]);
+        while(nextEven < nextOdd) {
             if ( A[nextEven] % 2 == 0) {
                 nextEven++;
             } else {
@@ -13,6 +16,6 @@ public class OddsOrEvens {
                 A[nextOdd--] = temp;
             }
         }
-        
+
     }
 }
